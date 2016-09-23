f1145d4f0c526c50c3ec8db61491ca01f5531b26
swathi-s <swathi.ehr@gmail.com>
2016-09-23 18:50:34 +0530

    Edit profile page is added

--- app/src/main/java/com/example/swathi/myproject/MainActivity.java
+++ app/src/main/java/com/example/swathi/myproject/MainActivity.java
@@ -27,91 +27,202 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    /**
+     * To hold the db object
+     */
     EmployeeDatabase employeeDatabase;
 
+    //to store the username
     String user;
 
+    //to store the user id
+    Long userID;
+
+    //to store the selected image
     ImageView selectImg;
 
+    //if user not selected any image, give this as default image
     Uri imageUri = Uri.parse("android.resource://com.example.swathi.myproject/drawable/profile");
 
-    List<EmployeeDatabase> Employees = new ArrayList<EmployeeDatabase>();
-    ArrayAdapter<EmployeeDatabase> employeeDatabaseArrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //to open the default page on launching of application
         setContentView(R.layout.activity_main);
+
+        //create object of employeeDatabase class
         employeeDatabase = new EmployeeDatabase(this);
+
+        //to get the writable instance of db
+        //@TODO : not used
         SQLiteDatabase db = employeeDatabase.getWritableDatabase();
     }
 
-
+    /**
+     * for login, user has to enter his username and password to login into the application
+     * @param view
+     */
     public void loginUser(View view) {
+
+        //get the username value from user
         EditText username = (EditText) findViewById(R.id.userName);
+
+        //get the password from user
         EditText password = (EditText) findViewById(R.id.password);
 
+        //convert the username value into string
         String uname = username.getText().toString();
+
+        //convert the password value into string
         String pword = password.getText().toString();
 
+        /**
+         * Check if username or password is given or not,
+         * if any one field is empty, show the appropriate error message
+         * else compare the username and password in DB
+         */
         if(TextUtils.isEmpty(uname) ||
                 TextUtils.isEmpty(pword)){
             Toast.makeText(this,"Please enter username and password", Toast.LENGTH_LONG).show();
         }
         else
         {
+            //get the writable instance of employeeDatabase db
             SQLiteDatabase db = employeeDatabase.getWritableDatabase();
 
-            String[] columns = {employeeDatabase.NAME,employeeDatabase.USERNAME,employeeDatabase.PASSWORD,employeeDatabase.IMAGE};
+            //list the required columns
+            String[] columns = {employeeDatabase.UID,employeeDatabase.IMAGE};
+
+            //list the selection arguments for where clause
             String[] selectionArgs = {uname,pword};
 
+            /**
+             * execute the query by using db.query()
+             * this function will take multiple arguments
+             * 1. Table name
+             * 2. columns
+             * 3. selection string
+             * 4. selection arguments
+             * 5. group by
+             * 6. having
+             * 7. order by
+             * 8. limit
+             */
             Cursor cursor = db.query(employeeDatabase.TABLE_NAME,columns, employeeDatabase.USERNAME +"=? AND "+employeeDatabase.PASSWORD +" =? " , selectionArgs,null,null,null,null);
             Log.d("cursor", String.valueOf(cursor.getColumnNames()));
+
+            //if user exists
             if(cursor != null && cursor.getCount()> 0) {
 
+                //iterate through all the values
                 while (cursor.moveToNext()) {
+
+                    //get the index of image column
                     int imgIndex = cursor.getColumnIndex(employeeDatabase.IMAGE);
+
                     Log.d("index", String.valueOf(imgIndex));
 
+                    /**
+                     * get the value from db based on column index
+                     *
+                     * get the image path from db
+                     */
                     String uri = cursor.getString(imgIndex);
-                   // int imgindex = cursor.getColumnIndex(employeeDatabase.IMAGE);
+
+                    // int imgindex = cursor.getColumnIndex(employeeDatabase.IMAGE);
                     //String uri = cursor.getString(imgindex);
+
+                    //convert the value from string to image uri
                     imageUri = Uri.parse(uri);
+
+                    //get the column index of "UID" column
+                    int idIndex = cursor.getColumnIndex(employeeDatabase.UID);
+
+                    //get the userid
+                    userID = cursor.getLong(idIndex);
                 }
 
+                //set the user name
                 user = uname;
                 //setContentView(R.layout.successfull_login_layout);
+
+                //call the dashboard function , to load the default profile page for user
                 dashboard();
 
             }
             else
             {
+                /**
+                 * If username or password is invalid, then show the erro message
+                 */
                 Toast.makeText(this, "Give Valid Username and Password", Toast.LENGTH_LONG).show();
             }
 
         }
     }
 
+    /**
+     * to signup
+     *
+     * this method will load the sinup page for user
+     *
+     * @param view
+     */
     public void signupUser(View view) {
 
+        //set the view page
         setContentView(R.layout.signup_layout);
 
+        //get the image
         selectImg = (ImageView) findViewById(R.id.enterImg);
+
+        //and provide onclick functionality to select the image from galary
         selectImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //create Intent object
                 Intent intent = new Intent();
+
+                //set the type as image
                 intent.setType("image/*");
+
+                //set the action as Intent.ACTION_GET_CONTENT
                 intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                /**
+                 * Starting another activity doesn't have to be one-way.
+                 * You can also start another activity and receive a result back.
+                 * To receive a result, call startActivityForResult() (instead of startActivity()).
+                 */
+                //use startActivityForResult() to return the selected image to the application
                 startActivityForResult(Intent.createChooser(intent,"select image"),1);
             }
         });
 
     }
 
+    /**
+     * to get the result from activity
+     * this method has 3 arguments
+     * 1. The request code you passed to startActivityForResult().
+     *
+     * 2. A result code specified by the second activity.
+     * This is either RESULT_OK if the operation was successful or
+     * RESULT_CANCELED if the user backed out or the operation failed for some reason
+     *
+     * 3. An Intent that carries the result data.
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if( resultCode == RESULT_OK) {
             if(requestCode == 1) {
+                //if we got the result, then set that to image uri
                 imageUri = data.getData();
                 selectImg.setImageURI(data.getData());
             }
@@ -119,12 +230,80 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         //super.onActivityResult(requestCode, resultCode, data);
     }
 
+    /**
+     * To save the user details in DB
+     * @param view
+     */
     public void saveUser(View view) {
+
+        //get the name of user
         EditText nameTxt = (EditText) findViewById(R.id.enterName);
+
+        //get the username
         EditText usernameTxt = (EditText) findViewById(R.id.enterUserName);
+
+        //get the password
         EditText passwordTxt = (EditText) findViewById(R.id.enterPassword);
 
+        //get the image, @TODO : not used
         ImageView selectedImg = (ImageView) findViewById(R.id.enterImg);
+
+        //convert nameTxt to string
+        String name = nameTxt.getText().toString();
+
+        //convert userNameTxt to string
+        String uname = usernameTxt.getText().toString();
+
+        //convert passwordTxt to string
+        String pword = passwordTxt.getText().toString();
+
+        //convert imageUri to string
+        String selctedImg = imageUri.toString();
+
+        //check if any of the fields are empty
+        if(TextUtils.isEmpty(name) || TextUtils.isEmpty(uname) || TextUtils.isEmpty(pword))
+        {
+            //if any field is empty then show the error message
+            Toast.makeText(this,"all the fields are required",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            /**
+             * If all the fileds are provided by the user,
+             * then insert that into DB
+             */
+
+            //get the writable instance of employee database db
+            SQLiteDatabase db = employeeDatabase.getWritableDatabase();
+
+            //
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(employeeDatabase.NAME,name);
+            contentValues.put(employeeDatabase.USERNAME,uname);
+            contentValues.put(employeeDatabase.PASSWORD,pword);
+            contentValues.put(employeeDatabase.IMAGE,selctedImg);
+
+            long id = db.insert(employeeDatabase.TABLE_NAME,null,contentValues);
+            Toast.makeText(this,name + "has been inserted into DB ",Toast.LENGTH_SHORT).show();
+            if(id > 0)
+            {
+                user = uname;
+                userID = id;
+                dashboard();
+            }
+        }
+    }
+
+    public void updateUser(View view) {
+
+        Log.d("user id",userID.toString());
+
+        String[] whereArgs = {userID.toString()};
+        EditText nameTxt = (EditText) findViewById(R.id.editName);
+        EditText usernameTxt = (EditText) findViewById(R.id.editPassword);
+        EditText passwordTxt = (EditText) findViewById(R.id.editPassword);
+
+        ImageView selectedImg = (ImageView) findViewById(R.id.editImg);
 
         String name = nameTxt.getText().toString();
         String uname = usernameTxt.getText().toString();
@@ -143,13 +322,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             contentValues.put(employeeDatabase.PASSWORD,pword);
             contentValues.put(employeeDatabase.IMAGE,selctedImg);
 
-            long id = db.insert(employeeDatabase.TABLE_NAME,null,contentValues);
-            Toast.makeText(this,name + "has been inserted into DB ",Toast.LENGTH_SHORT).show();
-            if(id > 0)
-            {
-                user = uname;
-                dashboard();
-            }
+            //long id = db.insert(employeeDatabase.TABLE_NAME,null,contentValues);
+            db.update(employeeDatabase.TABLE_NAME,contentValues,employeeDatabase.UID+ " =? ",whereArgs);
+
+            Toast.makeText(this,name + "has been updated successfully ",Toast.LENGTH_SHORT).show();
+
+            dashboard();
+
         }
     }
 
@@ -157,6 +336,62 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
     }
 
+    public void editProfile()
+    {
+        setContentView(R.layout.edit_profile);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.options,android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        EditText editName = (EditText) findViewById(R.id.editName);
+        EditText editUserName = (EditText) findViewById(R.id.editUserName);
+        EditText editPassword = (EditText) findViewById(R.id.editPassword);
+        selectImg = (ImageView) findViewById(R.id.editImg);
+        selectImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"select image"),1);
+            }
+        });
+
+        String[] columns = {employeeDatabase.NAME,employeeDatabase.USERNAME,employeeDatabase.PASSWORD,employeeDatabase.IMAGE};
+        String[] selectionArgs = {user};
+        SQLiteDatabase db =  employeeDatabase.getWritableDatabase();
+        Cursor cursor = db.query(employeeDatabase.TABLE_NAME,columns,employeeDatabase.USERNAME +"=?",selectionArgs,null,null,null,null);
+
+        if(cursor.getCount() > 0)
+        {
+            while(cursor.moveToNext())
+            {
+                int nameIndex = cursor.getColumnIndex(employeeDatabase.NAME);
+                String nameValue = cursor.getString(nameIndex);
+                editName.setText(nameValue);
+
+                int usernameIndex = cursor.getColumnIndex(employeeDatabase.USERNAME);
+                String userNameValue = cursor.getString(usernameIndex);
+                editUserName.setText(userNameValue);
+
+                int passwordIndex = cursor.getColumnIndex(employeeDatabase.PASSWORD);
+                String passwordValue = cursor.getString(passwordIndex);
+                editPassword.setText(passwordValue);
+
+                int imageIndex = cursor.getColumnIndex(employeeDatabase.IMAGE);
+                String imageValue = cursor.getString(imageIndex);
+                selectImg.setImageURI(Uri.parse(imageValue));
+
+
+                //editName.setText();
+            }
+        }
+
+    }
     public void dashboard()
     {
 
@@ -203,6 +438,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         {
             Log.d("options","entered into dashboard");
             dashboard();
+        }
+        else if(option.equals("Edit Profile"))
+        {
+            Log.d("options","entered into edit profile");
+            editProfile();
         }
 
     }
